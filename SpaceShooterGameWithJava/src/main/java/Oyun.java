b84556bd4c837557ba5cb0e2b92fa3a27596bdeb
burakTuran00 <burakturan7410@gmail.com>
2023-10-25 14:02:30 +0300

    SpaceShooterGameWithJava

--- SpaceShooterGameWithJava/src/main/java/Oyun.java
+++ SpaceShooterGameWithJava/src/main/java/Oyun.java
@@ -14,9 +14,10 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageInputStream;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-class Fire
-{
+class Fire {
+
     private int x;
     private int y;
 
@@ -42,60 +43,72 @@ class Fire
     }
 }
 
-public class Oyun extends JPanel implements KeyListener, ActionListener
-{
+public class Oyun extends JPanel implements KeyListener, ActionListener {
+
+    Timer timer = new Timer(5, this);
+
     private int time = 0;
     private int ammoo = 0;
     private BufferedImage image;
-    private ArrayList<Fire> atesler = new ArrayList<Fire>();  
-    
+    private ArrayList<Fire> atesler = new ArrayList<Fire>();
+
     private int fireDirY = 1;
-    
+
     private int topX = 0;
     private int topDirX = 2;
-    
+
     private int uzayGemisiX = 0;
     private int dirUzayX = 20;
 
-    public Oyun() 
-    {
-        try 
-        {
+    public Oyun() {
+        try {
             image = ImageIO.read(new FileImageInputStream(new File("uzaygemisi.png")));
-        } 
-        catch (IOException ex) 
-        {
+        } catch (IOException ex) {
             Logger.getLogger(Oyun.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         setBackground(Color.BLACK);
-        
-        
+
+        timer.start();
     }
 
     @Override
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         super.paint(g);
-        
+
         g.setColor(Color.RED);
         g.fillOval(topX, 0, 20, 20);
-        
+
         g.drawImage(image, uzayGemisiX, 490, image.getWidth() / 10, image.getHeight() / 10, this);
     }
 
     @Override
-    public void repaint() 
-    {
-     
+    public void repaint() {
+        super.repaint();
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
+        int c = e.getKeyCode();
+
+        if (c == KeyEvent.VK_LEFT) {
+            if (uzayGemisiX <= 0) {
+                uzayGemisiX = 0;
+            } else {
+                uzayGemisiX -= dirUzayX;
+            }
+        } else if (c == KeyEvent.VK_RIGHT) {
+            if (uzayGemisiX >= 720) {
+                uzayGemisiX = 720;
+            } else {
+                uzayGemisiX += dirUzayX;
+            }
+        }
     }
 
     @Override
@@ -104,6 +117,16 @@ public class Oyun extends JPanel implements KeyListener, ActionListener
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        topX += topDirX;
+
+        if (topX >= 720) {
+            topDirX = -topDirX;
+        }
+        if (topX <= 0) {
+            topDirX = -topDirX;
+        }
+
+        repaint();
     }
-    
+
 }
