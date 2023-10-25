9fb65d01e30a01faae86c62f0811e53bbecfb9a2
burakTuran00 <burakturan7410@gmail.com>
2023-10-25 13:11:49 +0300

    SpaceShooterGameWithJava

---
+++ SpaceShooterGameWithJava/src/main/java/com/mycompany/spaceshootergamewithjava/SpaceShooterGameWithJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.spaceshootergamewithjava;
+
+/**
+ *
+ * @author burak
+ */
+public class SpaceShooterGameWithJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
